d9371ed7d6d461d418236dca1679e4ac54b972d2
Aadil1228 <aadilgoyal989@gmail.com>
2025-02-01 23:10:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from extensions import db
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db.init_app(app)
+
+@app.route("/")
+def index():
+    return render_template('index.html', activePage='index')
+
+@app.route("/about")
+def about():
+    return render_template('about.html', activePage='about')
+
+if __name__ == "__main__":
+    app.run(debug=True)
