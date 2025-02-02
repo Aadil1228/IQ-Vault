7087fed68f72069cd6aa6ad9c80d6be51a920051
Aadil1228 <aadilgoyal989@gmail.com>
2025-02-02 16:10:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,17 @@ from extensions import db
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+app.config['SQLALCHEMY_BINDS'] = {
+    'users_db': 'sqlite:///users_db.sqlite3',
+    'quiz_db': 'sqlite:///quiz_db.sqlite3'
+}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db.init_app(app)
 
 @app.route("/")
 def index():
+    
     return render_template('index.html', activePage='index')
 
 @app.route("/about")
